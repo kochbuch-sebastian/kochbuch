39637e55b798c6aaac3f761e9331ba730273fe3c
Sebastian Thomas <sebastian_34260@web.de>
2020-04-27 12:44:59 +0200

    Added somefiles.

---
+++ client/src/recipes/ShowRecipes.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <p class="error" v-if="error">{{ this.error }}</p>
+    <div v-show="items.length === 0">Keine Rezepte gefunden!</div>
+    <div v-show="items.length !== 0">Anzahl Rezepte: {{ items.length }}</div>
+
+    <div v-if="showUnderEachOther">
+      <ul
+        v-for="(recipe, index) in items"
+        v-show="items.length > 0"
+        :item="recipe"
+        :index="index"
+        :key="recipe._id"
+      >
+        <li>
+          <router-link
+            id="recipes"
+            class="item router-links"
+            :to="{ name: 'Recipe', params: { id: recipe._id } }"
+          >{{ recipe.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="!showUnderEachOther">
+      <router-link
+        id="recipes"
+        class="item router-links"
+        v-for="(recipe, index) in items"
+        v-show="items.length > 0"
+        :item="recipe"
+        :index="index"
+        :key="recipe._id"
+        :to="{ name: 'Recipe', params: { id: recipe._id } }"
+      >{{ recipe.title }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowRecipes',
+  data() {
+    return {
+      error: '',
+    };
+  },
+  computed: {
+    showUnderEachOther() {
+      return window.innerWidth < 500;
+    },
+  },
+  props: ['items'],
+};
+</script>
+
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
