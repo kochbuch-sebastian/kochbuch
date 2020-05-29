0cf510dfec773f23587a508ad8f73b0773fba668
Sebastian Thomas <sebastian_34260@web.de>
2020-05-29 15:33:02 +0200

    Added push notification support, does it really work?

---
+++ server/config/pushKeys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  vapIDKeys: {
+    publicKey:
+      'BJ7W-pBAXF91XktUlW4smzlr5DKSn3HZI5ubRO2FL9xzvo3s5r0duXXKCH1o6MWgegXat4JT7uM0eooeYO0xpzE',
+    privateKey: 'NMC_I-O-k_rqJG-vf1eapL4Ity5pWJPe0FXd64gbhmM',
+  },
+};
