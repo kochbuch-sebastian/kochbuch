b2f7f2ea0a37110d251fa588a45f92450a9cc66c
Sebastian Thomas <sebastian_34260@web.de>
2021-01-10 14:14:40 +0100

    Fixed bugs

---
+++ server/public/precache-manifest.81d43a519dea2d456619929d91486dab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d736800acdb0b86361",
+    "url": "/css/app.b4906366.css"
+  },
+  {
+    "revision": "6cd59e6f017813280fa81b81fc7f91b9",
+    "url": "/img/delete_icon.6cd59e6f.png"
+  },
+  {
+    "revision": "de61f60264d216c85999961d1b1838e4",
+    "url": "/img/favorite_icon.de61f602.svg"
+  },
+  {
+    "revision": "8bedd6805be55b3651ddb37ee6a2575a",
+    "url": "/img/search_icon.8bedd680.png"
+  },
+  {
+    "revision": "7949e65849c70684607bda04ed408a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d736800acdb0b86361",
+    "url": "/js/app.9492f348.js"
+  },
+  {
+    "revision": "43f42fc44f4b57c6c553",
+    "url": "/js/chunk-vendors.c82c71ff.js"
+  },
+  {
+    "revision": "fbf6f8e3383901358c9102ad5ecb4af8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
