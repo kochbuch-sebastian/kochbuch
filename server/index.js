3423ab052fffb527403d33c4c05cee633d2ac543
Sebastian Thomas <kontakt@sthomas.ch>
2022-11-07 13:39:38 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // DB Config
 const db = process.env.MONGO_URI;
